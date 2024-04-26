1b74c3a9e35264683af9794f336ca06a8cc18363
FabioPenedo <fabiopenedo21@gmail.com>
2024-04-26 16:58:47 -0300

    primeiro commit, exercicio-excecoes

---
+++ App/Entities/Account.cs
@@ -0,0 +1,39 @@
+﻿using App.Entities.Exceptions;
+
+namespace App.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; } = string.Empty;
+        public double Balance { get; private set; }
+        public double WithDrawLimit { get; private set; }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (Balance == 0 || amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,41 @@
+﻿using App.Entities;
+using App.Entities.Exceptions;
+using System.Globalization;
+
+namespace App // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine()!);
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine()!;
+                Console.Write("Initial Balance: ");
+                double initialBalance = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+                Account account = new(number, holder, initialBalance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+                account.WithDraw(amount);
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException error)
+            {
+                Console.WriteLine("Withdraw error: " + error.Message);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Unexpected error: " + error.Message);
+            }
+        }
+    }
+}
